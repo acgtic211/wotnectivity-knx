6d8f6f717ceeef367e3ee7431e22b10a0d9b6517
Manel Mena <manel.mena@ual.es>
2020-02-04 11:24:37 +0100

    Initial release

--- README.md
+++ README.md
@@ -1,2 +1,67 @@
-# wotnectivity-knx
-Implementation of WoTnectivity Requester to manage KNX requests
+# WoTnectivity-knx
+Implementation of WoTnectivity Requester to manage KNX requests.
+
+## Install WoTnectivity-knx
+
+> :warning: **Requirements**: You need to have installed [Java 11+](https://openjdk.java.net/projects/jdk/11/) and [Maven 3.6.3+](http://maven.apache.org).
+
+If you want to compile your own source code, you will need to add the generated .jar to the local mvn repository.
+
+```console
+foo@bar:~$ mvn install:install-file "-Dfile=wotnectivity-knx-0.0.1-ALPHA-SNAPSHOT.jar" "-DgroupId=es.ual.acg" "-DartifactId=wotnectivity-knx" "-Dversion=0.0.1-ALPHA-SNAPSHOT" "-Dpackaging=jar"
+```
+
+After installing it to your local maven repository you will only need to add it to the dependencies of the project where you want to use it.
+
+```xml
+<dependency>
+    <groupId>es.ual.acg</groupId>
+    <artifactId>wotnectivity-knx</artifactId>
+    <version>0.0.1-ALPHA-SNAPSHOT</version>
+</dependency>
+```
+
+## Use Example
+
+In the next fragment of code you can see a use case of `KnxReq`.
+
+```java
+String address = "192.168.1.62";
+String payload = "0.1";
+KnxReq tester = new KnxReq();
+
+var configuration = new HashMap<String, Object>();
+
+configuration.put("requestType", "write");
+configuration.put("dataType", "5.001");
+configuration.put("group", "1/0/1");
+
+try{
+    this.tester.sendRequest(address, configuration, payload)
+    var response = "Payload written correctly";
+}catch(Exception e){
+    System.out.println(e.getMessage());
+}
+```
+
+The configuration parameter of the request needs to have at least three parameters: 
+
+* `requestType` where the type of request will be declared. The request type can be of three different types:
+
+    * `read`: Sends a request to read a value in the group address.
+    * `write`: Write a value in the declared group address.
+    * `subscribe`: Subscribes to the group address executing the behaviour of the listener declared.
+
+* `group`: Defines the group address of the particular interaction that we want to execute.
+
+* `dataType`: Declares the data type of the group address. To see the compatible dataTypes please visit [Calimero Core Library](https://github.com/calimero-project/calimero-core).
+
+The example request is done with the default listener that is implemented in `KnxListener.java`. This listener just shows in the console the changes in the group address subscribed. This implementation is very straight forward, if you want to give your application other behaviour when listening messages in the queue you have to implement a clase like that one and declare your class when you instantiate the `KnxReq`.
+
+```java
+KnxReq tester = new KnxReq(new YourListenerKnx());
+```
+
+## Disclaimer
+
+This library was developed with the [Calimero project](https://github.com/calimero-project/introduction) as a base.
--- src/main/java/es/ual/acg/KnxReq.java
+++ src/main/java/es/ual/acg/KnxReq.java
@@ -1,7 +1,8 @@
 package es.ual.acg;
 
-
 import java.net.InetSocketAddress;
+import java.util.HashMap;
+import java.util.concurrent.CompletableFuture;
 
 import es.ual.acg.utils.KnxProcessListener;
 import tuwien.auto.calimero.GroupAddress;
@@ -16,128 +17,160 @@ import tuwien.auto.calimero.process.ProcessCommunicator;
 import tuwien.auto.calimero.process.ProcessCommunicatorImpl;
 import tuwien.auto.calimero.process.ProcessListener;
 
-public class KnxReq {
-
+public class KnxReq implements IRequester {
 
 	InetSocketAddress remote;
 	ProcessListener pl;
 
-	
-	/** 
-	 * @return 
+	/**
+	 * @return
 	 */
-	public KnxReq(){
+	public KnxReq() {
 		this.pl = new KnxProcessListener();
 	}
 
-	public KnxReq(ProcessListener pl){
+	public KnxReq(ProcessListener pl) {
 		this.pl = pl;
 	}
 
-	
-	/** 
+	/**
 	 * @param address
 	 * @param group
 	 * @param datatype
 	 * @return String
 	 * @throws Exception
 	 */
-	public String getStatus(String address, String group, String datatype) throws Exception {
-	
+	public String readStatus(String address, String group, String datatype) throws Exception {
+
 		this.remote = new InetSocketAddress(address, 3671);
 		// Create our network link, and pass it to a process communicator
-		try (KNXNetworkLink knxLink = KNXNetworkLinkIP.newTunnelingLink(null,this.remote, false, TPSettings.TP1);
-				
-			ProcessCommunicator pc = new ProcessCommunicatorImpl(knxLink)) {
+		try (KNXNetworkLink knxLink = KNXNetworkLinkIP.newTunnelingLink(null, this.remote, false, TPSettings.TP1);
+
+				ProcessCommunicator pc = new ProcessCommunicatorImpl(knxLink)) {
 
 			System.out.println("read value from datapoint " + group);
 
 			GroupAddress groupAddress = new GroupAddress(group);
 
-			
-
-			// Uncomment the next line, if you want to write back the same value to the KNX network
+			// Uncomment the next line, if you want to write back the same value to the KNX
+			// network
 			// pc.write(group, value);
 
-			final Datapoint dp = new StateDP(groupAddress, "my datapoint "+groupAddress.toString());
-			
+			final Datapoint dp = new StateDP(groupAddress, "my datapoint " + groupAddress.toString());
 
 			System.out.println(DPTXlator8BitSigned.DPT_VALUE_1_UCOUNT.getID());
-			//dp.setDPT(0, KnxUtils.getTranslatorDPT(String value));
+			// dp.setDPT(0, KnxUtils.getTranslatorDPT(String value));
 
 			dp.setDPT(0, datatype);
 			String result = pc.read(dp).toString();
 
 			System.out.println("datapoint " + group + " value = " + result);
-			
-			//JCR: Review
+
+			// JCR: Review
 			pc.close();
 			knxLink.close();
-			
+
 			return result;
-		}
-		catch (KNXException | InterruptedException e) {
+		} catch (KNXException | InterruptedException e) {
 			throw new Exception("Error accessing KNX datapoint: " + e.getMessage());
 		}
 	}
 
-	public void subscribeToKNXBuffer(String address, String group, String datatype) throws Exception{
+	public void subscribeToKNXBuffer(String address, String group, String datatype) throws Exception {
 
 		final InetSocketAddress remote = new InetSocketAddress(address, 3671);
 		try (KNXNetworkLink knxLink = KNXNetworkLinkIP.newTunnelingLink(null, remote, false, TPSettings.TP1);
-		     ProcessCommunicator pc = new ProcessCommunicatorImpl(knxLink)) {
+				ProcessCommunicator pc = new ProcessCommunicatorImpl(knxLink)) {
 
 			// start listening to group notifications using a process listener
 			pc.addProcessListener(pl);
 			System.out.println("Monitoring KNX network using KNXnet/IP server " + address + " ...");
 
-			while (knxLink.isOpen()) Thread.sleep(1000);
-		}
-		catch (final KNXException | InterruptedException | RuntimeException e) {
+			while (knxLink.isOpen())
+				Thread.sleep(1000);
+		} catch (final KNXException | InterruptedException | RuntimeException e) {
 			System.err.println(e);
 		}
-			
 
 	}
-	
-	/** 
+
+	/**
 	 * @param address
 	 * @param group
 	 * @param datatype
 	 * @param value
 	 * @throws Exception
 	 */
-	public void setStatus(String address, String group, String datatype, String value) throws Exception {
-	
+	public void writeStatus(String address, String group, String datatype, String value) throws Exception {
+
 		this.remote = new InetSocketAddress(address, 3671);
-		//final InetSocketAddress local = new InetSocketAddress(localip, 0);
+		// final InetSocketAddress local = new InetSocketAddress(localip, 0);
 		// Create our network link, and pass it to a process communicator
 		try (KNXNetworkLink knxLink = KNXNetworkLinkIP.newTunnelingLink(null, this.remote, false, TPSettings.TP1);
-			
-			ProcessCommunicator pc = new ProcessCommunicatorImpl(knxLink)) {
+
+				ProcessCommunicator pc = new ProcessCommunicatorImpl(knxLink)) {
 
 			System.out.println("read value from datapoint " + group);
 
 			GroupAddress groupAddress = new GroupAddress(group);
 
-			// Uncomment the next line, if you want to write back the same value to the KNX network
+			// Uncomment the next line, if you want to write back the same value to the KNX
+			// network
 			// pc.write(group, value);
 
-			final Datapoint dp = new StateDP(groupAddress, "my datapoint "+groupAddress.toString());
-			
-			//dp.setDPT(0, KnxUtils.getTranslatorDPT(String value));
+			final Datapoint dp = new StateDP(groupAddress, "my datapoint " + groupAddress.toString());
+
+			// dp.setDPT(0, KnxUtils.getTranslatorDPT(String value));
 
 			dp.setDPT(0, datatype);
 			pc.write(dp, value);
 
-			//JCR: Review
+			// JCR: Review
 			pc.close();
 			knxLink.close();
 
-		}
-		catch (KNXException | InterruptedException e) {
+		} catch (KNXException | InterruptedException e) {
 			throw new Exception("Error accessing KNX datapoint: " + e.getMessage());
 		}
+	}
+
+	@Override
+	public CompletableFuture<?> sendRequest(String address, HashMap<String, Object> configuration, Object payload) {
+
+		String requestType = configuration.get("requestType").toString();
+		configuration.remove("requestType");
+		CompletableFuture<?> output = new CompletableFuture<String>();
+		if (requestType.equals("read")) {
+			output = CompletableFuture.supplyAsync(() -> {
+				try {
+					return readStatus(address, configuration.get("group").toString(),
+							configuration.get("dataType").toString());
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				return "Error in request";
+			});
+        }else if (requestType.equals("write")){
+            output = CompletableFuture.runAsync(()->{
+				try {
+					writeStatus(address, configuration.get("group").toString(), configuration.get("datatype").toString(), payload.toString());
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			});
+        }else if (requestType.equals("subscribe")){
+			output = CompletableFuture.runAsync(()->{
+				try {
+					subscribeToKNXBuffer(address, configuration.get("group").toString(), configuration.get("datatype").toString());
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			});
+		}else{
+            output = null;
+        }
+        return output;
+
 	}
 		
 }
